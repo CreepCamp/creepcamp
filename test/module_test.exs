9927e7ec4421e87c401e0fef9eede649e8de60fd
Barcaioni Bastien <bastien.barcaioni@gmail.com>
2015-04-27 21:22:30 +0200

    ajout du fichier d'exemple neuron.ex, ajout du test pour les modules encore vide, update du test sur les fonctions

---
+++ test/module_test.exs
@@ -0,0 +1,14 @@
+defmodule ModuleTest do 
+  use ExUnit.Case
+  
+  # Vous trouverez dans le dossier exemples/bases/ un fichier appelé neuron.ex
+  # Dedans ce trouve la definition actuelle d'un genotype de neurone
+  # Nous reviendrons plus en detail sur le pourquoi du comment plus tard
+  # Mais nous allons utilisé cette base pour les explications relative au modules.
+
+  # Le module que l'ont va testé s'appel Genotype.Neuron
+
+
+
+
+end
